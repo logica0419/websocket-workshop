641a224abcfecb06d96cccb23a3b42cda3e1b11c
logica <84759675+logica0419@users.noreply.github.com>
2022-02-10 06:34:20 +0900

    initialized frontend

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img class="icon" alt="WebSocket Logo" src="../assets/websocket.svg" />
+  <h1>WebSocket Client</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss">
+#app {
+  max-height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-top: 50px;
+  font-size: calc(5px + 2vmin);
+  min-width: 400px;
+}
+
+.icon {
+  width: min(50vw, 30vh);
+}
+</style>
