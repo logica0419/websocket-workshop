5f231dc15094985078a999cd12a249ed1c8a1c23
logica <84759675+logica0419@users.noreply.github.com>
2022-02-10 07:37:24 +0900

    initialized backend

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Logger.SetLevel(log.DEBUG)
+	e.Logger.SetHeader("${time_rfc3339} ${prefix} ${short_file} ${line} |")
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Format: "${time_rfc3339} method = ${method} | uri = ${uri} | code = ${status} ${error}\n"}))
+
+	e.Static("/", "public")
+
+	api := e.Group("/api")
+	{
+		api.GET("/ping", func(c echo.Context) error {
+			return c.String(http.StatusOK, "pong")
+		})
+	}
+
+	e.Logger.Panic(e.Start(":8080"))
+}
