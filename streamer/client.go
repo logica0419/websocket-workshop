838fad4311f90f78662cb2d094a3c2c685ca5026
logica <84759675+logica0419@users.noreply.github.com>
2022-02-11 15:06:00 +0900

    setup websocket client

---
+++ streamer/client.go
@@ -0,0 +1,53 @@
+package streamer
+
+import (
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	id       uuid.UUID
+	roomID   string
+	conn     *websocket.Conn
+	listener *chan string
+	sender   chan string
+	closer   chan bool
+}
+
+func newClient(roomID string, conn *websocket.Conn, listener *chan string) *client {
+	return &client{
+		id:       uuid.Must(uuid.NewV4()),
+		roomID:   roomID,
+		conn:     conn,
+		listener: listener,
+		sender:   make(chan string),
+	}
+}
+
+func (c *client) listen() {
+	for {
+		msgType, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			c.closer <- true
+			return
+		}
+		if msgType != websocket.TextMessage {
+			continue
+		}
+
+		msgStr := string(msg)
+		*c.listener <- msgStr
+	}
+}
+
+func (c *client) send() {
+	for {
+		msg := <-c.sender
+
+		err := c.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		if err != nil {
+			c.closer <- true
+			return
+		}
+	}
+}
