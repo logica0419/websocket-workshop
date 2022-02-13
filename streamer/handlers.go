5f2cccfea0a8c10bc379ec2781b4ede4a529cf59
logica <84759675+logica0419@users.noreply.github.com>
2022-02-14 04:08:43 +0900

    setup handler

---
+++ streamer/handlers.go
@@ -0,0 +1,33 @@
+package streamer
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type body struct {
+	Method string                 `json:"method,omitempty"`
+	Args   map[string]interface{} `json:"data,omitempty"`
+}
+
+func (s *Streamer) handleWebSocket(data receiveData) error {
+	var req body
+	err := json.Unmarshal(data.body, &req)
+	if err != nil {
+		return err
+	}
+
+	switch req.Method {
+	case "message":
+		s.sendToRoom(data.roomID, req.Args["message"].(string))
+	case "time":
+		s.sendToRoom(data.roomID, time.Now().String())
+	default:
+		log.Printf("unknown method: %s", req.Method)
+		return fmt.Errorf("unknown method: %s", req.Method)
+	}
+
+	return nil
+}
