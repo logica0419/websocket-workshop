29bdd008be5e2428382dc23da901ade34afb5c2d
logica <84759675+logica0419@users.noreply.github.com>
2022-02-17 17:10:53 +0900

    implemented message list

---
+++ frontend/src/components/MessageList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <p v-for="message in messages" :key="message" class="message">
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    messages: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  text-align: left;
+  padding: 0 20px;
+  width: min(80vw, 500px);
+  height: min(50vh, 300px);
+  overflow-y: auto;
+  border: 3px solid #000000;
+  border-radius: 15px;
+}
+
+.message {
+  margin: 0.3em 0;
+  font-size: calc(7.5px + 2vmin);
+}
+</style>
