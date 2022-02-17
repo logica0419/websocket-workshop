f7e59ddb9d099a44674156f6125972936c295476
logica <84759675+logica0419@users.noreply.github.com>
2022-02-17 17:12:17 +0900

    included transceive function to app

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <img class="icon" alt="WebSocket Logo" src="../assets/websocket.svg" />
-  <h1>WebSocket Client</h1>
+  <div class="app">
+    <img class="icon" alt="WebSocket Logo" src="../assets/websocket.svg" />
+    <h1>WebSocket Client</h1>
+    <transceive-container />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import TransceiveContainer from "./TransceiveContainer.vue";
 
-export default defineComponent({});
+export default defineComponent({
+  components: { TransceiveContainer },
+});
 </script>
 
-<style lang="scss">
-#app {
+<style scoped lang="scss">
+.app {
   max-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
   color: #2c3e50;
   padding-top: 50px;
   font-size: calc(5px + 2vmin);
