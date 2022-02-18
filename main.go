d4940dd4d82841c61a9f60fc05b69433321f5b1f
logica <84759675+logica0419@users.noreply.github.com>
2022-02-18 17:58:10 +0900

    fixed package name

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
-	"github.com/logica0419/websocket_workshop/streamer"
+	"github.com/logica0419/websocket-workshop/streamer"
 )
 
 func main() {
